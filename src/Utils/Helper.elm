1f6352e1595009928494f0725a6ddfd9ae8369f1
SimonHauck <simonhauck@gmx.de>
2019-07-03 22:23:45 +0200

    implement foldr2/foldl2 helper functions to iterate over rotors

---
+++ src/Utils/Helper.elm
@@ -0,0 +1,57 @@
+module Utils.Helper exposing (foldl2, foldr2, testFunction, testFunctionHelper)
+
+import Enigma.Rotor exposing (..)
+
+
+{-| Reduce the given list from the left and perform the given method on th element a.
+The neutral element will be required at the beginning and at the end of the list
+-}
+foldl2 : (a -> b -> b -> a) -> a -> List b -> b -> a
+foldl2 f input list neutralElement =
+    foldl2Helper f input list neutralElement neutralElement
+
+
+foldl2Helper : (a -> b -> b -> a) -> a -> List b -> b -> b -> a
+foldl2Helper f input list neutralElement previousElement =
+    case list of
+        [] ->
+            f input neutralElement previousElement
+
+        x :: xs ->
+            foldl2Helper f (f input x previousElement) xs neutralElement x
+
+
+{-| Reduce the given list from the right and perform the given method on th element a.
+The neutral element will be required at the beginning and at the end of the list
+-}
+foldr2 : (a -> b -> b -> a) -> a -> List b -> b -> a
+foldr2 f input list neutralElement =
+    foldl2 f input (List.reverse list) neutralElement
+
+
+testFunction : Char -> Maybe Char
+testFunction input =
+    let
+        rotor3Test =
+            { rotor3 | currentPosition = 1 }
+
+        rotor2Test =
+            { rotor2 | currentPosition = 3 }
+
+        rotor1Test =
+            { rotor1 | currentPosition = 5 }
+
+        rotorList =
+            [ rotor3Test, rotor2Test, rotor1Test, staticRotor ]
+    in
+    foldl2 testFunctionHelper (Just 'A') rotorList staticRotor
+
+
+testFunctionHelper : Maybe Char -> Rotor -> Rotor -> Maybe Char
+testFunctionHelper inputChar currentRotor prevRotor =
+    case inputChar of
+        Nothing ->
+            Nothing
+
+        Just a ->
+            substituteCharacter (Debug.log "test " a) currentRotor prevRotor ToReflector
